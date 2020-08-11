38436d4a9af53188608003849ac681fd09fc3e55
Askar <askarsm93@gmail.com>
2020-08-11 21:47:47 +0500

    Initial commit

---
+++ src/main/java/FibonacciRow.java
@@ -0,0 +1,40 @@
+public class FibonacciRow {
+
+    public static void main(String[] args) {
+        fibonacciIter(10); // вызов метода вычисления фибоначчи итарционно
+
+        for(int i=0; i<10; i++){
+            System.out.print(fibonacciRec(i) + ", "); // вызов метода вычисления фибоначчи рекурсионно
+        }
+
+
+    }
+
+    public static void fibonacciIter(int number){
+        int firstNumber = 0;
+        int nextNumber = 1;
+        int bufferedNumber;
+        System.out.print(firstNumber + ", ");
+        for (int i = 1; i<number; i++ ){
+            System.out.print(nextNumber + ", ");
+            bufferedNumber = nextNumber;
+            nextNumber += firstNumber;
+            firstNumber = bufferedNumber;
+
+        }
+    }
+
+    public static int fibonacciRec(int n){
+        if (n==1) {
+            return 1;
+        }
+        else if (n==0) {
+            return 0;
+        }
+
+        else {
+            return fibonacciRec(n - 1) + fibonacciRec(n - 2);
+        }
+
+    }
+}
